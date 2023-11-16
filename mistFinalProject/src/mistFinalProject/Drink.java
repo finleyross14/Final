159c5cbfd490c86531869b30657b16ce85696644
jordanwheat <jcw38756@uga.edu>
2023-11-16 13:29:58 -0500

    11/16/23

--- mistFinalProject/src/mistFinalProject/Drink.java
+++ mistFinalProject/src/mistFinalProject/Drink.java
@@ -1,6 +1,26 @@
 package mistFinalProject;
 
 public class Drink {
-	//hi
-	//jordan
+	private String drinkName;
+	private int drinkSize;
+	
+	public void setName(String newName) {
+		drinkName = newName;
+	}
+
+	public void setSize(int newSize) {
+	drinkSize = newSize;	
+	}
+	
+	public void printItem() {
+		System.out.println(drinkName + " " + drinkSize);
+	}
+}
+public class Tea extends Drink {
+	private int percentSweetener;
+}
+public class Coffee extends Drink {
+private int numShots;
+
+}
 }
