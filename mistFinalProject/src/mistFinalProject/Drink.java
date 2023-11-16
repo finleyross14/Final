feb1168e35d2f95e7f591f8d47536a6d658a25d4
jordanwheat <jcw38756@uga.edu>
2023-11-16 13:51:17 -0500

    array

--- mistFinalProject/src/mistFinalProject/Drink.java
+++ mistFinalProject/src/mistFinalProject/Drink.java
@@ -4,6 +4,13 @@ public class Drink {
 	private String drinkName;
 	private int drinkSize;
 	
+	// static array of all toppings          
+	private String[] size = new String[3]; {
+		size[1]= "12 oz";
+		size[2]= "16 oz";
+		size[3]= "24 oz";
+	}
+	
 	public void setName(String newName) {
 		drinkName = newName;
 	}
@@ -15,4 +22,11 @@ public class Drink {
 	public void printItem() {
 		System.out.println(drinkName + " " + drinkSize);
 	}
+	
+
+	
+
+	
+	//create topping
+	
 }
