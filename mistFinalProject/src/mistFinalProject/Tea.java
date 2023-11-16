f83736df70b58b937402857de2dad8bcdf219618
jordanwheat <jcw38756@uga.edu>
2023-11-16 13:34:02 -0500

    j

--- mistFinalProject/src/mistFinalProject/Tea.java
+++ mistFinalProject/src/mistFinalProject/Tea.java
@@ -1,5 +1,13 @@
 package mistFinalProject;
 
-public class Tea {
+public class Tea extends Drink {       
+	private int percentSweetener;
 
-}
+	public int getPercentSweetener() {
+		return percentSweetener;
+	}
+
+	public void setPercentSweetener(int percentSweetener) {
+		this.percentSweetener = percentSweetener;
+	}      
+}                                      
