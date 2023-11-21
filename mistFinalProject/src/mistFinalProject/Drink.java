97e8090dccc772d3f89fd0c56d4ca96bd716b58f
finleyross14 <finley.ross14@gmail.com>
2023-11-21 13:27:22 -0500

    array

--- mistFinalProject/src/mistFinalProject/Drink.java
+++ mistFinalProject/src/mistFinalProject/Drink.java
@@ -3,6 +3,8 @@ package mistFinalProject;
 public class Drink {
 	private String drinkName;
 	private int drinkSize;
+	private static final int[] sizes = {12, 16, 24};
+	
 	
 	public void setName(String newName) {
 		drinkName = newName;
