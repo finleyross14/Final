5f958f0ebb48af62c9d597c6af881144ed9220bd
finleyross14 <finley.ross14@gmail.com>
2023-11-26 19:05:00 -0500

    added validation loops for all inputs

--- mistFinalProject/src/mistFinalProject/Tester.java
+++ mistFinalProject/src/mistFinalProject/Tester.java
@@ -55,16 +55,27 @@ public class Tester {
 			System.out.println("Please input 12, 16, or 24.");
 			int s = scnr.nextInt();
 			scnr.nextLine();
+			if(s != Drink.sizes[0] && s != Drink.sizes[1] && s != Drink.sizes[2]) {
+				while(s != Drink.sizes[0] && s != Drink.sizes[1] && s != Drink.sizes[2]) {
+					System.out.println("Invalid input, please input a valid size.");
+					s = scnr.nextInt();
+					
+				}
+			}
 			drink.setDrinkSize(s);
 			System.out.println("How many shots of espresso?");
 			System.out.println("Please input a number 1-4: ");
 			int e = scnr.nextInt();
 			scnr.nextLine();
+			if(e < 1 || e > 4) {
+				while(e < 1 || e > 4) {
+					System.out.println("Invalid input, please input a valid number of espresso shots.");
+					e = scnr.nextInt();
+				}
+			}
 			drink.setNumShots(e);
 			double total = drink.priceDrink();
 			System.out.println("Your total is: $" + total);
-
-		
 		}
 		
 		//Order logic for tea
@@ -73,35 +84,47 @@ public class Tester {
 			System.out.println("What is your drink of choice? (Please type the exact name case sensitive)");
 			String drinkOrder = scnr.nextLine();
 			//Testing for if drink exists
-			/*if(Drink.menu.keySet().contains(drinkOrder) != true) {
+			if(Drink.menu.keySet().contains(drinkOrder) != true) {
 				while(Drink.menu.keySet().contains(drinkOrder) != true) {
 					System.out.println("Invalid input, please type the exact name case sensitive.");
 					drinkOrder = scnr.nextLine();
 				}
-			}*/
+			}
 			drink.setDrinkName(drinkOrder);
 			System.out.println("What size would you like: 12oz, 16oz, or 24oz?");
 			System.out.println("Please input 12, 16, or 24.");
 			int s = scnr.nextInt();
 			scnr.nextLine();
+			if(s != Drink.sizes[0] && s != Drink.sizes[1] && s != Drink.sizes[2]) {
+				while(s != Drink.sizes[0] && s != Drink.sizes[1] && s != Drink.sizes[2]) {
+					System.out.println("Invalid input, please input a valid size.");
+					s = scnr.nextInt();
+				}
+			}
 			drink.setDrinkSize(s);
 			System.out.println("What topping would you like??");
 			System.out.println("(Please type the exact name case sensitive, type 'No Topping' for no topping.) ");
 			String t = scnr.nextLine();
+			if(Drink.menu.keySet().contains(t) != true) {
+				while(Drink.menu.keySet().contains(t) != true) {
+					System.out.println("Invalid input, please type the exact name case sensitive.");
+					t = scnr.nextLine();
+				}
+			}
 			drink.setTopping(t);
 			System.out.println("What percent sweet?");
 			System.out.println("Please input a number 0-100: ");
 			int e = scnr.nextInt();
 			scnr.nextLine();
+			if(e < 0 || e > 100) {
+				while(e < 0 || e > 100) {
+					System.out.println("Invalid input, please input a valid number of espresso shots.");
+					e = scnr.nextInt();
+				}
+			}
 			drink.setPercentSweet(e);
 			double total = drink.priceDrink();
 			System.out.println("Your total is: $" + total);
 		}
-		
-		
-
 	}
-	
-	
-
 }
